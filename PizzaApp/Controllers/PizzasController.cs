7ade5f6454705288561c4b66cdd8c2a5888771e3
Anna Żak <s16485@pjwstk.edu.pl>
2020-01-22 16:44:52 +0100

    Rozbudowa kontrolera PizzasController

--- PizzaApp/Controllers/PizzasController.cs
+++ PizzaApp/Controllers/PizzasController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PizzaApp.Models;
 
 namespace PizzaApp.Controllers
@@ -37,6 +38,48 @@ namespace PizzaApp.Controllers
 
             return Ok(pizzaCala);
         }
+
+        [HttpPost]
+        public IActionResult Create( PizzaCala newPizzaCala)
+        {
+            _context.PizzaCala.Add(newPizzaCala);
+            _context.SaveChanges();
+
+            return StatusCode(201, newPizzaCala);
+        }
+
+        [HttpPut]
+        public IActionResult Update(PizzaCala updatedPizzaCala)
+        {
+      
+            if (_context.PizzaCala.Count(p => p.IdGotowaPizza == updatedPizzaCala.IdGotowaPizza) == 0)
+            {
+                return NotFound();
+            }
+
+            _context.PizzaCala.Attach(updatedPizzaCala);
+            _context.Entry(updatedPizzaCala).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(updatedPizzaCala);
+
+        }
+        
+        [HttpDelete("{idGotowaPizza:int}")]
+        public IActionResult Delete(int idPizzaCala)
+        {
+            var pizza = _context.PizzaCala.FirstOrDefault(p => p.IdGotowaPizza == idPizzaCala);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            _context.PizzaCala.Remove(pizza);
+            _context.SaveChanges();
+
+            return Ok(pizza);
+        }
+
 
     }
 }
